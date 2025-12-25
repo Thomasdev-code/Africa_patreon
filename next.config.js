6131c1e0745cecb255f5620373efb8444c1176dd
thomas <kariukithomas34@gmail.com>
2025-12-25 13:41:37 -0800

    Fix Vercel build: force Webpack and adjust Next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // Explicitly disable Turbopack (we're using Webpack)
-  turbopack: {},
-
   // Correct way to exclude Prisma from bundling in Next 16
   serverExternalPackages: ['@prisma/client'],
 
@@ -19,19 +16,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-  },
-
-  // Webpack config (safe since we force --webpack)
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-        },
-      }
-    }
-    return config
   },
 }
 
